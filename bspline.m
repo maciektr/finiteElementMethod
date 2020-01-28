19bb7053b25fd3279f73a2cf6949f60a20050116
Maciej <maciek2tr99@gmail.com>
2020-01-29 00:54:01 +0100

    [Update] Remove old comments

--- bspline.m
+++ bspline.m
@@ -15,27 +15,10 @@ e4 = @(x) y(x,4);
 % fplot(e1);
 e = @(k) (@(x) y(x,k));
 
-
-% int = @(f,n,k) (1/n)*(f(1/(n*sqrt(3))+(2*k-1)/n)+f(-1/(n*sqrt(3))+(2*k-1)/n));
 int = @(f,k) (1/2)*(f(1/(2*sqrt(3))+(k)/2)+f(-1/(2*sqrt(3))+(k)/2));
 
-% b = @(u,v,n,k) (diff(u,1)*v(1)-u(0)*v(0)+int(@(x)diff(u,x)*diff(v,x),n,k) +2*int(@(x)diff(u,x)*diff(v,x),n,k+1));
 b = @(u,v) (diff(u,1)*v(1)-u(0)*v(0)+int(@(x)diff(u,x)*diff(v,x),1) +2*int(@(x)diff(u,x)*diff(v,x),3));
 % b(e1,e1);
-
-%b = @(u,v,upvp,up, int,n,k) (up(1)*v(1)-up(0)*v(0)+((2*k/n <= 1) .* int(upvp,n,k))+2*(((2*k-2)/n >= 1) .* int(upvp,n,k+1)))
-%bn = @(n,k1,k2) b(@(x) y(x,n,k1), @(x) y(x,n,k2), (@(x) yp(x,n,k1) * yp(x,n,k2)), @(x) yp(x,n,k1),int, n,min(k1,k2))
-% b(e1,e2,(@(x)ep1(x)*ep2(x)),ep1,int,4,1)
-% bn(4,4,3)
-% bn(4,1,2)
-% bn(4,2,2)
-% bn(4,2,1)
-
-% dp = @(fun,x) ((fun(x + 0.000001) - fun(x - 0.000001)) / (2 * 0.000001))
-
-% dp(e2,1)
-% bn = @(n,k1,k2) b(@(x) y(x,n,k1), @(x) y(x,n,k2), (@(x) dp(@(x) y(x,n,k1),x) * dp(@(x) y(x,n,k2),x)),@(x) dp(@(x) y(x,n,k1),x) ,int, n,min(k1,k2))
-% bn(4,2,2)
 
 B = sparse(N,N);
 for k = 1:N
